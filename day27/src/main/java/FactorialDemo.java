3e6f37f816057d6a3b825d11528c968f3ba31c13
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-09-03 21:07:29 +0530

    add

---
+++ day27/src/main/java/FactorialDemo.java
@@ -0,0 +1,15 @@
+public class FactorialDemo {
+    public static void main(String[] args) {
+        m1();
+        System.out.println("main");
+    }
+
+    private static void m1() {
+        System.out.println("m1");
+        m2();
+    }
+
+    private static void m2() {
+        System.out.println("m2");
+    }
+}
